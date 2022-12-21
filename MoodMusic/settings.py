24167220f666a39da3bb436d26b8444eb6ac9dae
DKP <iamdkp7531@gmail.com>
2022-12-21 13:10:22 +0530

    changing something

--- MoodMusic/settings.py
+++ MoodMusic/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'MoodMusic.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
         
-    }
+    # }
 }
 
 
