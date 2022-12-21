aebd821112e21b7e952ef3e186999ecffb9ac154
DKP <iamdkp7531@gmail.com>
2022-12-21 12:02:21 +0530

    command error setting

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.10.0 -m pip install - requirements.txt
-python3.10.0 manage.py collectstatic --noinput --clear
+python3.10 -m pip install - requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END"
