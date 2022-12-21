7141227f83120e1bc218c2852062b1aa455c0aff
DKP <iamdkp7531@gmail.com>
2022-12-21 12:54:04 +0530

    command error setting5

--- MoodMusic/settings.py
+++ MoodMusic/settings.py
@@ -125,7 +125,9 @@ STATIC_URL = 'static/'
 
 # manual code 
 STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
-STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build','static')
+# STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT =BASE_DIR / 'staticfiles_build' / 'static'
+
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
