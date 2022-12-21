0e440c607a07a91072caa4c0bc7046d9931f1fd7
DKP <iamdkp7531@gmail.com>
2022-12-21 12:25:30 +0530

    command error setting4

--- MoodMusic/urls.py
+++ MoodMusic/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('music/',include('Music.urls')),
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header = "Login-App Admin"
 admin.site.site_title = "Login-App Admin Portal"
