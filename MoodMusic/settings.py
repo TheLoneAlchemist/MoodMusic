a263a75946eb4223f959b5eb5bde0cd03d49d9ba
DKP <iamdkp7531@gmail.com>
2022-12-22 12:15:05 +0530

    Render Deployment Allowed Host setting

--- MoodMusic/settings.py
+++ MoodMusic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5g=nplhks5c@1ja_bwic=7bf@3ajitkohv9%jk6j8y4jtja9#s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
+ALLOWED_HOSTS = ['moodmusic.onrender.com','127.0.0.1']
 
 
 # Application definition
