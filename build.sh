73858cdae2214eadd7f9d4aafa97021ef84c6477
DKP <iamdkp7531@gmail.com>
2022-12-22 19:14:18 +0530

    Render Deployment Admin setup

--- build.sh
+++ build.sh
@@ -8,4 +8,6 @@ pip install -r requirements.txt
 
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'email', 'Admin@00#')" | python manage.py shell
